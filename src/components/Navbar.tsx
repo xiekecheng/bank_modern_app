cc5aceccc278ab4369c848cf3fb0d74350bed179
xkccoding <xkccoding@gmail.com>
2022-11-09 22:13:50 +0800

    feat: 编写Navbar组件

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
-import {logo} from '../assets'
+import React, {useState} from 'react';
+import {close, logo, menu} from '../assets'
+import {navLinks} from '../constants'
 
 const Navbar = () => {
-    return (
-        <div className={`flex justify-between items-center`}>
-            <div>
-                <img src={logo} alt=""/>
-            </div>
-            <ul className={`flex gap-4 text-white`}>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Features</li>
-                <li>Support</li>
-            </ul>
-
+  const [toggle, setToggle] = useState(false)
+  return (
+    <nav className={`flex justify-between items-center navbar py-6 w-full`}>
+      <div>
+        <img src={logo} alt="" className={`w-[124px] h-[32px]`}/>
+      </div>
+      <ul className={`gap-4 text-white hidden sm:flex`}>
+        {
+          navLinks.map(item => (
+            <li key={item.id}>
+              <a href={`${item.id}`}>{item.title}</a>
+            </li>
+          ))
+        }
+      </ul>
+      <div className="sm:hidden flex">
+        <img src={toggle ? close : menu} alt="menu" onClick={() => setToggle(!toggle)}
+             className={`object-contain w-[28px] h-[28px] cursor-pointer `}/>
+        <div className={`${toggle ? `flex` : `hidden`} absolute top-20 right-0`}>
+          <ul className={`gap-4 flex flex-col`}>
+            {
+              navLinks.map(item => (
+                <li key={item.id} className={`font-medium font-poppins`}>
+                  <a href={`#${item.id}`}>{item.title}</a>
+                </li>
+              ))
+            }
+          </ul>
         </div>
-    );
+      </div>
+
+    </nav>
+  );
 };
 
 export default Navbar;
